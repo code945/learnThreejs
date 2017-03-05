bd9bc3cd3cefb925b12ea618e564ea63161702fa
code945 <lhx880619@163.com>
2017-03-05 11:07:39 +0800

    binary loader

---
+++ showBinary/converted.js
@@ -0,0 +1,30 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "1.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 26952,
+        "faces"         : 52502,
+        "normals"       : 27505,
+        "uvs"           : 32982,
+        "materials"     : 1
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Material__0",
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "ceshi01.jpg",
+	"opacity" : 1.0,
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0
+	}],
+
+    "buffers": "converted.bin"
+
+}
